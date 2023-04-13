f696fb241a4ede834cfd5df51ce464bf22a0e9c4
Andrew Mason <andrewmcodes@protonmail.com>
2023-04-13 12:16:53 -0700

    chore: initial commit

---
+++ lib/turbo_debug.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "turbo_debug/version"
+
+module TurboDebug
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/turbo_debug_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe TurboDebug do
+  it "has a version number" do
+    expect(TurboDebug::VERSION).not_to be nil
+  end
+end
